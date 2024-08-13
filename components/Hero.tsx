cb5fc984c4952fca96ac9ae8b32f0face9a778ee
Geoffrey Sie <siegeoffrey@gmail.com>
2024-08-13 17:44:02 +0100

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+        <div>
+            <Spotlight className='-top-40 -left-10 
+            md:-left-32 md:-top-20 h-screen' fill='white'/>
+            <Spotlight className='top-10 left-full 
+            h-[80vh] w-[50vw]' fill='purple'/>
+            <Spotlight className='top-28 left-80
+            h-[80vh] w-[50vw]' fill='blue'/>
+        </div>
+
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] 
+        bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+            {/* Radial gradient for the container to give a faded look */}
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center 
+            dark:bg-black-100 bg-white 
+            [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+
+            </div>
+
+        </div>
+
+        <div className = "flex justify-center relative my-20 z-10">
+            <div className='max-w-[79vw] md:max-w-xl lg:max-w-[50vw] flex flex-col items-center justify-center'>
+                <h2 className = "uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                    Hello, I'm Geoffrey
+                </h2>
+                <TextGenerateEffect 
+                    className='text-center text-[40px] md:text-5xl lg:text-6xl mb-4' 
+                    words='Web and AI/ML Developer using Modern Solutions'>
+
+                </TextGenerateEffect>
+
+                <p className = "uppercase tracking-widest text-xs text-center max-w-90">
+                    Currently a Loughborough University student on placement year studying Computer Science and AI
+                </p>
+
+                <a href="#about">
+                    <MagicButton 
+                    title="Show my work"
+                    icon={<FaLocationArrow />}
+                    position = "right"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
