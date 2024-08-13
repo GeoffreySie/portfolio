83d345a6e8892fee9422d2afe2002cbb9f25da7b
Geoffrey Sie <siegeoffrey@gmail.com>
2024-08-13 19:50:37 +0100

    Changed Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className='w-full mb-[100px] md:mb-[50px] pb-10' id='contact'>
         <div className='flex flex-col items-center'>
             <h1 className='heading lg:max-w-[36vw]'>
-                Always <span className='text-purple'>Improving</span>, All Day, Every Day
+                Always <span className='text-purple'>Improving</span>, Always <span className='text-purple'>Doing My Best</span>
             </h1>
             <a href='mailto:contact@siegeoffrey@gmail.com'>
                 <MagicButton 
--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "loughborough-university-cn",
-project: "javascript-nextjs",
+org: "gs-code",
+project: "portfolio-website",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
