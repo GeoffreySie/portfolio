72bd66ea9ee542d308401dedb5c07cb2a49c860f
Geoffrey Sie <siegeoffrey@gmail.com>
2024-08-14 23:22:11 +0100

    Added extra components

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import { FloatingNav } from "@/components/ui/floating-navbar";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import Footer from "@/components/Footer";
+import Projects from "@/components/Projects";
+import Education from "@/components/Education";
+import Experience from "@/components/Experience";
+
 
 export default function Home() {
   return (
@@ -26,6 +30,9 @@ export default function Home() {
         ]}/>
         <Hero />
         <Grid />
+        <Projects />
+        <Education />
+        <Experience />
         <Footer />
       </div>
     </main>
